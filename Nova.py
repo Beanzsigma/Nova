3f9f808d14e5dec776e1c2efc83acb5b47a06f09
Beanzsigma <162669334+Beanzsigma@users.noreply.github.com>
2026-05-09 16:21:28 -0700

    Added Clear function

--- Nova.py
+++ Nova.py
@@ -21,7 +21,10 @@ app=ctk.CTk()
 app.resizable(False, False)
 app.title("Nova")
 app.geometry('700x500')
-#def clear(canvas, canvas_img):
+def clear(canvas, canvas_img):
+    for item in canvas.find_all():
+        if item != canvas_img:
+            canvas.delete(item)
 def getpath(relative_path):
     try:
         base_path = sys._MEIPASS
@@ -33,20 +36,20 @@ FR_PRIVATE = 0x10
 def load_font(font_path):
     windll.gdi32.AddFontResourceExW(font_path, FR_PRIVATE, 0)
 app.iconbitmap(getpath("Images/GIFS/logo.ico"))
+frames = []
+gif = Image.open(getpath("Images/GIFS/greenglow.gif"))
+for frame in ImageSequence.Iterator(gif):
+        frame = frame.copy().convert('RGBA')
+        r, g, b, a = frame.split()
+        a = a.point(lambda x:x *1)
+        frame.putalpha(a)
+        frames.append(ImageTk.PhotoImage(frame.resize((700, 500))))
 def gifbg():
     global after_id 
     if after_id:
         app.after_cancel(after_id)
     for widget in app.winfo_children():
         widget.destroy()
-    frames  = []
-    gif = Image.open(getpath('Images/GIFS/greenglow.gif'))
-    for frame in ImageSequence.Iterator(gif):
-        frame = frame.copy().convert('RGBA')
-        r, g, b, a = frame.split()
-        a = a.point(lambda x:x *1)
-        frame.putalpha(a)
-        frames.append(ImageTk.PhotoImage(frame.resize((700, 500))))
     canvas = Canvas(app, width=700, height=500, highlightthickness=0, bd=0, bg="black")
     canvas.place(x=0, y=0)
     canvasbg = canvas.create_image(0, 0, anchor="nw")
@@ -74,17 +77,9 @@ def welcome():
     canvas.tag_bind(continuebtn, "<Leave>", leave)
     canvas.tag_bind(continuebtn2, '<Enter>', enter)
     canvas.tag_bind(continuebtn2, "<Leave>", leave)
+    canvas.tag_bind(continuebtn, "<Button-1>", lambda e: welcome())
+    canvas.tag_bind(continuebtn2, "<Button-1>", lambda e: welcome())
 welcome()
-
-
-
-
-
-
-
-
-
-
 
 
 
