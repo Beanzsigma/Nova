1b519bfc9d952eac5a2c582c39be3d3f2d074359
Beanzsigma <162669334+Beanzsigma@users.noreply.github.com>
2026-05-16 16:43:46 -0700

    Switching from openwakeword to speech_recognition due to issues

--- wake-up_word.py
+++ wake-up_word.py
@@ -1,8 +1,25 @@
 from openwakeword.model import Model
 import sounddevice as sd
+import time
 import numpy as np
 model = Model()
 samplerate = 16000
 chunksize = 1280
 lastrigger = 0
 cooldown = 4
+def callback(indata, frames, time_info, status):
+    global lastrigger
+    audio = indata.flatten().astype(np.int16)
+    prediction = model.predict(audio)
+    for wakeword, score in prediction.items():
+        if score > 0.5:
+            current = time.time()
+            if current - lastrigger >cooldown:
+                lastrigger = current
+                print("detect")
+stream = sd.InputStream(samplerate=samplerate, channels=1, dtype="int16", blocksize=chunksize, callback=callback)
+with stream:
+    print('i hear u bru')
+    while True:
+        sd.sleep(1000)
+
